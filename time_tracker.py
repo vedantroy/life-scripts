71b409f333149fb6854fc77b73161161ced9a4d3
Vedant Roy <vroy101@gmail.com>
2022-10-25 22:11:12 -0400

    fix some bugs

--- time_tracker.py
+++ time_tracker.py
@@ -29,19 +29,30 @@ def make_chart(path):
         end = None
         untracked = None
         for idx, row in enumerate(data[1:]):
+            def safe_parse(s):
+                try:
+                    # assert s[:-2] == 'AM' or s[:-2] == 'PM'
+                    return parser.parse(s)
+                except Exception as e:
+                    raise ValueError(f"Error parsing \"{s}\" in {path} at row {idx}, with message: {e}")
+                
             start_s, end_s = row[name['Start']], row[name['End']]
-            start = parser.parse(start_s)
+            start = safe_parse(start_s)
             if end:
                 untracked_cur = start - end
                 untracked = untracked + untracked_cur if untracked else untracked_cur
 
-            end = parser.parse(end_s)
+            end = safe_parse(end_s)
             delta = end - start
 
             if end < start:
                 delta += datetime.timedelta(days=1)
+	     
+            category = row[name['Category']].strip()
+            if delta > datetime.timedelta(hours=5):
+                print(f"Warning: {delta} is a long time for {category} in {path} at row {idx}. Start: \"{start_s}\", End: \"{end_s}\"")
 
-            category = row[name['Category']]
+            
             if category in category_to_time:
                 category_to_time[category] += delta
             else:
